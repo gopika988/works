8582e8e9c655d721c3be8c6a9bbbd68549acd6f1
Gopika Sabu <76676531+gopika988@users.noreply.github.com>
2022-06-09 23:56:42 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	</head>
+	<body>
+		<form action="" method="" name="">
+			<label>FirstName:<input type="text" name="name" id="lname" placeholder="please enter your firstname"></label>
+			<label>LastName:<input type="text" name="name" id="fname" placeholder="please enter your last name"></label>
+			<label>Age:<input type="date" name="age" id="age"></label>
+			<label>Emai:<input type="text" name="email" id"email" placeholder="enter your email"></label>
+			<label>gender:<input type="radio" name="gender" value="male" checked> Male
+                          <input type="radio" name="gender" value="female"> Female
+                      </label>
+                      <label for ="country">country:</label>label>
+                     
+  <select name="country" id="country">
+    <option value="australia">Australia</option>
+    <option value="uae">UAE</option>
+    <option value="india">India</option>
+    <option value="usa">USA</option>
+  </select>
+            <label>Address:<textarea id="address" name="address" rows="10" cols="20" placeholder="enter your address"></textarea>
+            	<input type="submit"><a href="oppage.php">
+
+			
+		</form>
+	</body>
